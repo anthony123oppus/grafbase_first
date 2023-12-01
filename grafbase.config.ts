bf26357d26db345862cd3157c774e292c5e2ef14
unknown <Anthony>
2023-12-01 20:29:53 +0800

    sanamugana

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,28 +1,26 @@
 import { g, config } from '@grafbase/sdk'
 
-g.type('Post', {
-  id: g.id(),
+const User = g.model('User', {
+  name:g.string(),
+  email:g.email(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedIn:g.url().optional(),
+  projects:g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Project', {
+  title:g.string(),
+  description:g.string(),
+  image:g.url(),
+  liveSiteUrl:g.url(),
+  githubUrl:g.url(),
+  category:g.string().search(),
+  createdBy:g.relation(() => User)
 })
 
 export default config({
   schema: g,
-  // Authentication - https://grafbase.com/docs/auth
-  auth: {
-    // OpenID Connect
-    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
-    // providers: [oidc],
-    rules: (rules) => {
-      rules.public()
-    },
-  },
-  // Caching - https://grafbase.com/docs/graphql-edge-caching
-  // cache: {
-  //   rules: [
-  //     {
-  //       types: ['Query'], // Cache everything for 60 seconds
-  //       maxAge: 60,
-  //       staleWhileRevalidate: 60
-  //     }
-  //   ]
-  // }
+
 })
